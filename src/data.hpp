cf27d086c39e5bba80ca60e0f88d47aad7337493
brute <ericgrochowicz@hotmail.com>
2025-09-16 17:15:27 -0300

    integra data com o resto. faz wrap. falta um pouco

--- src/data.hpp
+++ src/data.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <math>
+#include <math.h>
 #include "coordinate.hpp"
 
 struct Data {
@@ -7,6 +7,7 @@ struct Data {
     int group;
 
     Data(double _x, double _y, int _group) : x(_x), y(_y), group(_group) {}
+    Data() : x(0), y(0), group(0) {}
 
     double dist(Data oth) {
         double X = x - oth.x;
